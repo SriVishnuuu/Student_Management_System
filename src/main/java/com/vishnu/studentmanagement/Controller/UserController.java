e63ee4260a81063abca5e882b6d02427ef1cce70
Sri vishnu <srivishnugoud06@gmail.com>
2026-08-19 11:34:03 +0530

    Add root URL redirect to login

--- src/main/java/com/vishnu/studentmanagement/Controller/UserController.java
+++ src/main/java/com/vishnu/studentmanagement/Controller/UserController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/login";
+    }
+
+
     @GetMapping("/login")
     public String login(){
         return "login";
